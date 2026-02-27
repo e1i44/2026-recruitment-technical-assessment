331e981e52fab96b57adabc9b8d16cf5190de203
e1i44 <137380944+e1i44@users.noreply.github.com>
2026-02-27 11:08:58 +1100

    task 3 implementation (#3)

--- backend/py_template/devdonalds.py
+++ backend/py_template/devdonalds.py
@@ -21,7 +21,6 @@ class Recipe(CookbookEntry):
 class Ingredient(CookbookEntry):
 	cook_time: int
 
-
 # =============================================================================
 # ==== HTTP Endpoint Stubs ====================================================
 # =============================================================================
@@ -67,7 +66,7 @@ def create_entry():
 		return f'Cookbook already has an entry with name {name}', 400
 
 	type = data.get('type', '')
-	result: CookbookEntry | str
+	result: Union[CookbookEntry | str]
 	if type == 'recipe':
 		itemsdata = data.get('requiredItems', '')
 		result = create_recipe(name, itemsdata)
@@ -125,7 +124,7 @@ def getInt(input: str):
 
 
 # returns a cookbook item with a given name or None if it doesnt exist
-def search_cookbook(name: str):
+def search_cookbook(name: str) -> Union[CookbookEntry | None]:
 	items = [i for i in cookbook if i.name == name]
 	
 	if len(items) == 0:
@@ -138,8 +137,65 @@ def search_cookbook(name: str):
 # Endpoint that returns a summary of a recipe that corresponds to a query name
 @app.route('/summary', methods=['GET'])
 def summary():
-	# TODO: implement me
-	return 'not implemented', 500
+	name = request.args.get('name')
+	if search_cookbook(name) is None:
+		return 'Recipe with name not found', 400
+	if isinstance(search_cookbook(name), Ingredient):
+		return 'Can only summarise recipes, not ingredients', 400
+	
+	try:
+		cooktime = getCookTimeByName(name)
+	except Exception as e:
+		return f'Failed to summarise recipe due to issue with required items: {e}', 400
+
+	baseingredients = getBaseIngredients(name, 1)
+
+	return jsonify({
+		'name': name,
+		'cookTime': cooktime,
+		'ingredients': baseingredients
+	})
+
+
+def getCookTimeByName(name: str) -> int:
+	item: Union[ CookbookEntry | None ] = search_cookbook(name)
+
+	if isinstance(item, Recipe):
+		cooktime = 0
+		for i in item.required_items:
+			cooktime += getCookTimeByName(i.name) * i.quantity
+		return cooktime
+
+	elif isinstance(item, Ingredient):
+		return item.cook_time
+	
+	else:
+		raise Exception(f'{name} not found in cookbook') 
+
+
+def getBaseIngredients(name: str, quantity: int) -> List[RequiredItem]:
+	item: CookbookEntry = search_cookbook(name)
+	if isinstance(item, Recipe):
+		ingredients = []
+		addIngredients(ingredients, item.required_items, quantity)
+		return ingredients
+	else:
+		return [RequiredItem(name, quantity)]
+
+
+def addIngredients(ingredients: List[RequiredItem], new_ingredients: List[RequiredItem], multiplier: int):
+	for i in new_ingredients:
+		existingingredient = [j for j in ingredients if j.name == i.name]
+		if len(existingingredient) > 0:
+			existingingredient[0].quantity += i.quantity * multiplier
+			return
+
+		item = search_cookbook(i.name)
+
+		if isinstance(item, Recipe):
+			addIngredients(ingredients, getBaseIngredients(i.name, i.quantity * multiplier), 1)
+		else:
+			ingredients.append(RequiredItem(i.name, i.quantity * multiplier))
 
 
 # =============================================================================
